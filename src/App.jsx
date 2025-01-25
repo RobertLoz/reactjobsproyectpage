6d7ca869a75e4fd3a425ea074f0a93d73c5a4ede
RobertLoz <lozanoroberto1796@gmail.com>
2025-01-25 14:09:25 -0600

    Initial commit to upload project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <Hero  />
+    <HomeCards />
+    <JobListings />
+  
+
+    <section className="m-auto max-w-lg my-10 px-6">
+      <a
+        href="jobs.html"
+        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a>
+    </section>
+    </>
+  )
+}
+
+export default App
